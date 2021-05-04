f96bb4667a5385021f0d068cfa8c9c68427cf5b8
geo967 <geomathew967@gmail.com>
2021-05-04 12:12:12 +0530

    c1

---
+++ app/src/main/java/com/example/layouts/MainActivity2.java
@@ -0,0 +1,36 @@
+package com.example.layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button button2=findViewById(R.id.button2);
+        Button button1=findViewById(R.id.button1);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity2.this,MainActivity3.class);
+                startActivity(intent);
+
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/layouts/MainActivity3.java
@@ -0,0 +1,25 @@
+package com.example.layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        Button button1=findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity3.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
